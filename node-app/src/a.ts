5b16960578419eae594d774acf47a3557eee2eaa
Durgesh-05 <durgeshkumar.d@somaiya.edu>
2024-08-08 00:20:55 +0530

    implement fns to understand interfaces

--- node-app/src/a.ts
+++ node-app/src/a.ts
@@ -37,6 +37,7 @@ function printHelloWorld(): void {
 
 // Interfaces -Very Imp
 // It helps to remove duplicate code
+// It helps to structure object in type-safe manner
 interface User {
   firstName: string;
   lastName: string;
@@ -64,23 +65,80 @@ interface Person {
   greet(parameter: string): void;
 }
 
-class Employee implements Person {
-  name: string;
-  age: number;
+// class Employee implements Person {
+//   name: string;
+//   age: number;
 
-  constructor(name: string, age: number) {
-    this.name = name;
-    this.age = age;
-  }
+//   constructor(name: string, age: number) {
+//     this.name = name;
+//     this.age = age;
+//   }
 
-  greet(parameter: string): void {
-    console.log('hello' + parameter);
-  }
-}
+//   greet(parameter: string): void {
+//     console.log('hello' + parameter);
+//   }
+// }
 
-// Type - type and interface are almost the same
+// Type - type and interface are almost the same, In type we have to use "="
 // type User = {
 //   firstName: string,
 //   lastName: string,
 //   age: number,
 // }
+
+// Union
+type StringOrNumber = string | number;
+function printId(id: StringOrNumber): void {
+  console.log(typeof id);
+}
+
+// Intersection - if we want to create a type which is mixed of other types
+type Employee = {
+  name: string;
+  startDate: Date;
+};
+
+type Manager = {
+  name: string;
+  department: string;
+};
+
+// we have mixed 2 types to get new types
+type TeamLead = Employee & Manager;
+
+const teamLead: TeamLead = {
+  name: 'Durgesh',
+  startDate: new Date(),
+  department: 'Software Engineer',
+};
+
+function getMax(arr: number[]): number {
+  let max: number = -1;
+  arr.forEach((element) => (element > max ? (max = element) : max));
+  return max;
+}
+
+let arr: number[] = [1, 2, 4, 6, 3, 0];
+
+// console.log(getMax(arr));
+
+const user1: User = {
+  firstName: 'Durgesh',
+  lastName: 'Dubey',
+  age: 19,
+};
+const user2: User = {
+  firstName: 'Rahul',
+  lastName: 'Dhanak',
+  age: 20,
+};
+const user3: User = {
+  firstName: 'Arya',
+  lastName: 'Gami',
+  age: 17,
+};
+
+const userList: User[] = [user1, user2, user3];
+
+const filteredUserList = userList.filter((user) => user.age >= 18);
+console.log(filteredUserList);
