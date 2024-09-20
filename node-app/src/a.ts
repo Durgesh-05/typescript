f517241bd9b798dade5968bd5dc6383f4539d5ec
Durgesh-05 <durgeshkumar.d@somaiya.edu>
2024-09-21 00:29:57 +0530

    Added Advance TS API

--- node-app/src/a.ts
+++ node-app/src/a.ts
@@ -204,3 +204,115 @@ function firstEl<T>(arr: T[]): T {
 // firstEL<string>(["Durgesh",21,3]) This will give error
 console.log(firstEl(['Durgesh', 'Dubey']).toUpperCase());
 console.log(firstEl([1, 2, 4]));
+
+interface UserData {
+  id: string;
+  name: string;
+  age: number;
+  email: string;
+  password: string;
+}
+
+// Suppose if we want to update user data we make a db call
+// But we have to pass parameter or another interface right?
+// interface UpdateProps {
+//   name: string;
+//   age: number;
+//   email: string;
+// }
+// function updateUser(updateData: UpdateProps){
+//   // make db call
+// }
+// function updateUser(name: string, email: string, age: number): void{
+
+// }
+
+// But problem in both the case is mostly a fn should not have more than 5-6 params and if suppose we make changes in User Interface then we have to apply this in all the interface which referred it
+// To solve this problem we can use pick
+// Pick is use for both types and intefaces
+
+type UpdateProps = Pick<UserData, 'age' | 'email' | 'name'>;
+// now we can use this
+function updateUser(updateData: UpdateProps) {
+  const { name, email, age } = updateData;
+  console.log('Updated data ', email + name + age);
+  // const user = make db call now
+}
+
+// Partial: This will make your type optional but all attributes
+type UpdatePropsOptional = Partial<UpdateProps>;
+// In real life you will not confirmed which data user have to update this will help you there
+
+// if we want to make some part not changeable like some objects or arrays we should use readonly
+
+interface UserProfile {
+  readonly name: string;
+  readonly age: number;
+}
+
+const user: UserProfile = {
+  name: 'Durgesh Dubey',
+  age: 20,
+};
+// I cannot change it again
+// user.name = "Gangesh" This will throw error
+// Usecase: You can use this in Config files
+
+interface Config {
+  endpoint: string;
+  apiKey: string;
+}
+
+const config: Readonly<Config> = {
+  endpoint: 'http://projects.100xdevs.com',
+  apiKey: 'neheg78yt7gyVHTREDC^87GVH',
+};
+// This will help other developer to not change any configurations
+
+// Record: It is cleaner way to declare the object
+
+type Users = Record<string, { age: number; name: string }>;
+
+const users: Users = {
+  '_radyt67&': {
+    age: 20,
+    name: 'Durgesh Dubey',
+  },
+  '_rar3yt67&': {
+    age: 22,
+    name: 'Gangesh Dubey',
+  },
+};
+
+// To create a key value pair, there is another way which is of Map
+// This is slight cleaner approach
+const User = new Map();
+
+User.set('_radyt67', {
+  age: 20,
+  name: 'Durgesh Dubey',
+});
+
+const userDataFromId = User.get('_radyt67');
+
+// In a function that can accept several types of inputs but you want to exclude specific types from being passed to it.
+type Events = 'click' | 'scroll' | 'mousemove';
+type ExcludeEvent = Exclude<Events, 'scroll'>; // 'click' | 'mousemove'
+
+const handleEvent = (event: ExcludeEvent) => {
+  console.log(`Handling event: ${event}`);
+};
+
+handleEvent('click'); // OK
+
+// More Important :- Type Inference in ZOD
+// if we have already defined zod schema and if we are using the same types again and again without any reusability then it will look more redundant
+
+// const userProfileSchema = {
+//   name: z.string().min(1),
+//   email: z.email().string(1),
+//   password: z.string().min(8)
+// }
+
+// const FinalUserSchema = z.infer<typeof userProfileSchema>
+// Now we can use this in other places
